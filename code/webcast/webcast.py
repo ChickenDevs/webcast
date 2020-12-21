8588c077fcbd42241f838a4ede75c9d9ea297ca1
ChickenDevs <chickenthef1sh@gmail.com>
2020-12-20 17:03:32 -0700

    Update Raw Code

--- code/webcast/webcast.py
+++ code/webcast/webcast.py
@@ -20,45 +20,58 @@ try:
     path.isfile('/etc/webcast/webcast.conf')
 except Exception as err:
     logger.warning("Couldn't find config file, creating file with defaults")
-    configFile = {"url": "https://raw.githubusercontent.com/ChickenDevs/webcast/main/test-off.xml", "videoDelay": 0,
-                  "audioDelay": 1, "volume": 0, "preset": "veryfast"}
+    configFile = {"auto": "true", "url": "https://raw.githubusercontent.com/ChickenDevs/webcast/main/test-off.xml", "resolution": "1280x720",
+                  "videoDelay": 0,"audioDelay": 1, "volume": 0, "preset": "veryfast"}
     out = json.dumps(configFile)
     f = open(f"/etc/webcast/webcast.conf", "w").write(out)
 
 with open(f"/etc/webcast/webcast.conf") as f:
     config = json.load(f)
 
-# Get the Config file from the Church
-xml = get(config['url']).text
-logger.info("Successfully retrieved XML file")
+if config['auto'] == "true":
+    # Get the Config file from the Church
+    xml = get(config['url']).text
+    logger.info("Successfully retrieved XML file")
 
-# Video Bitrate
-_, videobitrate = xml.split("<video>")
-videobitrate, _ = videobitrate.split("</video>")
-_, videobitrate = videobitrate.split("<custom_bitrate>")
-videobitrate, _ = videobitrate.split("</custom_bitrate>")
-videobitrate = int(videobitrate)
-logger.info(f"Video Bitrate: {videobitrate}")
+    # Video Bitrate
+    _, videobitrate = xml.split("<video>")
+    videobitrate, _ = videobitrate.split("</video>")
+    _, videobitrate = videobitrate.split("<custom_bitrate>")
+    videobitrate, _ = videobitrate.split("</custom_bitrate>")
+    videobitrate = int(videobitrate)
+    logger.info(f"Video Bitrate: {videobitrate}")
 
-# Audio Bitrate
-_, audiobitrate = xml.split("<audio>")
-audiobitrate, _ = audiobitrate.split("</audio>")
-_, audiobitrate = audiobitrate.split("<custom_bitrate>")
-audiobitrate, _ = audiobitrate.split("</custom_bitrate>")
-audiobitrate = int(audiobitrate)
-logger.info(f"Audio Bitrate: {audiobitrate}")
+    # Audio Bitrate
+    _, audiobitrate = xml.split("<audio>")
+    audiobitrate, _ = audiobitrate.split("</audio>")
+    _, audiobitrate = audiobitrate.split("<custom_bitrate>")
+    audiobitrate, _ = audiobitrate.split("</custom_bitrate>")
+    audiobitrate = int(audiobitrate)
+    logger.info(f"Audio Bitrate: {audiobitrate}")
 
-# RTMP URL
-_, url = xml.split("<url>")
-url, _ = url.split("</url>")
-logger.info(f"RTMP URL: {url}")
+    # RTMP URL
+    _, url = xml.split("<url>")
+    url, _ = url.split("</url>")
+    logger.info(f"RTMP URL: {url}")
 
-# Stream Password
-_, streamkey = xml.split("<stream>")
-streamkey, _ = streamkey.split("</stream>")
-logger.info(f"RMTP Key: {streamkey}")
+    # Stream Password
+    _, streamkey = xml.split("<stream>")
+    streamkey, _ = streamkey.split("</stream>")
+    logger.info(f"RMTP Key: {streamkey}")
+else:
+    videobitrate = config['vbitrate']
+    logger.info(f"Video Bitrate: {videobitrate}")
+    
+    audiobitrate = config['abitrate']
+    logger.info(f"Audio Bitrate: {audiobitrate}")
 
-if url.startswith("rtmp:"):  # A stream should be active
+    url = config['rtmpurl']
+    logger.info(f"RTMP URL: {url}")
+
+    streamkey = config['rtmpkey']
+    logger.info(f"RMTP Key: {streamkey}")
+
+if url.startswith("rtmp"):  # A stream should be active
     shouldberunning = True
     logger.info("Found Stream URL")
 else:
@@ -67,22 +80,22 @@ else:
 
 if system("ps -e | grep ffmpeg") == 0:  # ffmpeg is running
     running = True
-    logger.info("Found ffmpeg running")
+    logger.info("ffmpeg running")
 else:
     running = False
-    logger.info("ffmpeg not found")
+    logger.info("ffmpeg not running")
 
 if shouldberunning and running:
     logger.info("ffmpeg is already streaming")  # Stream is running
 
 if shouldberunning and not running:
     logger.info("Attemping to start ffmpeg")
-    system(f"ffmpeg -hide_banner   -f v4l2 -thread_queue_size 16 -framerate 25 "
-           "-video_size 1280x720 -input_format mjpeg -i /dev/video0 "
+    system(f"ffmpeg -hide_banner -f v4l2 -thread_queue_size 16 -framerate 25 "
+           f"-video_size {config['resolution']} -input_format mjpeg -i /dev/video0 "
            f"-f alsa -thread_queue_size 1024 -itsoffset {config['audioDelay']} -i sysdefault:CARD=Device "
            "-map 0:v -map 1:a -vcodec h264 -acodec aac "
-           f"-ar {audiobitrate} -ac 2 -preset {config['preset']} -b:v {videobitrate} -framerate 25 "
-           f"-filter:a volume={config['volume']}dB -f flv {url}/{streamkey} &")
+           f"-ar {audiobitrate} -ac 2 -preset {config['preset']} -b:v {videobitrate} -framerate 25 -g 50 "
+           f"-filter:a volume={config['volume']}dB -f flv '{url}/{streamkey}' &")
     time.sleep(5)
     if system("ps -e | grep ffmpeg") == 0:  # ffmpeg is running
         logger.info("Successfully started ffmpeg")
--- code/webcastconfig/webcastconfig.js
+++ code/webcastconfig/webcastconfig.js
@@ -1,4 +1,15 @@
+const auto = document.getElementById("auto");
 const url = document.getElementById("url");
+const urlTag = document.getElementById("urlTag");
+const rtmpurl = document.getElementById("rtmpurl");
+const rtmpurlTag = document.getElementById("rtmpurlTag");
+const rtmpkey = document.getElementById("rtmpkey");
+const rtmpkeyTag = document.getElementById("rtmpkeyTag");
+const vbitrate = document.getElementById("vbitrate");
+const vbitrateTag = document.getElementById("vbitrateTag");
+const abitrate = document.getElementById("abitrate");
+const abitrateTag = document.getElementById("abitrateTag");
+const resolution = document.getElementById("resolution")
 const videoDelay = document.getElementById("videoDelay")
 const audioDelay = document.getElementById("audioDelay")
 const volume = document.getElementById("volume")
@@ -15,9 +26,30 @@ const file = cockpit.file("/etc/webcast/webcast.conf", {syntax: JSON})
 file.read()
 .then((content, tag) => {
 	try {
+		if ('auto' in content) {
+			if (content.auto == "true") {
+				auto.checked = true
+			} else {auto.checked = false}
+		} else {auto.checked = true}
+		set_auto()
 		if ('url' in content) {
 			url.value = content.url
 		} else {url.value = "https://raw.githubusercontent.com/ChickenDevs/webcast/main/test-off.xml"}
+		if ('rtmpurl' in content) {
+			rtmpurl.value = content.rtmpurl
+		}
+		if ('rtmpkey' in content) {
+			rtmpkey.value = content.rtmpkey
+		}
+		if ('vbitrate' in content) {
+			vbitrate.value = content.vbitrate
+		} else {vbitrate.value = 1250000}
+		if ('abitrate' in content) {
+			abitrate.value = content.abitrate
+		} else {abitrate.value = 48000}
+		if ('resolution' in content) {
+			resolution.value = content.resolution
+		} else {resolution.value = "1280x720"}
 		if ('videoDelay' in content) {
 			videoDelay.value = content.videoDelay
 		} else {videoDelay.value = 0}
@@ -37,18 +69,26 @@ file.read()
 function write() {
 	try {
 		const data = {
+			auto: auto.checked.toString(),
 			url: url.value,
+			rtmpurl: rtmpurl.value,
+			rtmpkey: rtmpkey.value,
+			vbitrate: parseInt(vbitrate.value),
+			abitrate: parseInt(abitrate.value),
+			resolution: resolution.value,
 			videoDelay: parseFloat(videoDelay.value),
 			audioDelay: parseFloat(audioDelay.value),
 			volume: parseInt(volume.value),
 			preset: preset.value
 		}
+		console.log(data)
 		file.replace(data)
-		.then(tag => {console.log(tag)})
+		file.replace(data)
+		.then(tag => {success()})
 		.catch(err => {console.log(err); failure()})
-		success()
 	} catch(err) {
 		failure()
+		console.log(data)
 	}
 }
 
@@ -67,27 +107,115 @@ function failure() {
 }
 
 function service_ctl(action) {
-	let input
-	if (action === "status" || action === "restart") {
-		input = ["sudo", "systemctl", action, "webcast"]
-	} else { input = ["sudo", "systemctl", action, "webcast.timer"]}
-
 	output.innerHTML = "";
 
-	cockpit.spawn(input)
-	.stream(service_output)
-	.then(success())
-	.catch(err => {console.log(err)})
+        switch(action){
+		case "stop":
+			cockpit.spawn(["sudo", "pkill", "ffmpeg"])
+			.then()
+			cockpit.spawn(["sudo", "systemctl", "stop", "webcast.timer"])
+			.then(success())
+			break;
+		case "start":
+			cockpit.spawn(["sudo", "systemctl", "start", "webcast"])
+			.stream(service_output)
+			.then()
+			cockpit.spawn(["sudo", "systemctl", "start", "webcast.timer"])
+			.then(success())
+			break;
+		case "restart":
+			cockpit.spawn(["sudo", "pkill", "ffmpeg"])
+			.then()
+			cockpit.spawn(["sudo", "systemctl", "restart", "webcast"])
+			.stream(service_output)
+			.then(success())
+			break;
+		default:
+			cockpit.spawn(["sudo", "systemctl", "status", "webcast"])
+			.stream(service_output)
+			.then(success())
+	}
+
+	//.catch(err => {console.log(err)})
 }
 
 function service_output(data) {
 	output.append(document.createTextNode(data));
 }
 
+function set_presets() {
+	let veryfast = new Option("veryfast","veryfast");
+	let faster = new Option("faster", "faster");
+	let fast = new Option("fast", "fast");
+	let medium = new Option("medium", "medium");
+	if(resolution.value == "640x480"){
+		while (preset.options.length > 2) {
+			preset.remove(2)
+		}
+		preset.add(veryfast,undefined);
+		preset.add(faster,undefined);
+		preset.add(fast,undefined);
+		preset.add(medium,undefined);
+		preset.value = "fast";
+	}
+	if(resolution.value == "800x600") {
+		while (preset.options.length > 2) {
+			preset.remove(2)
+		}
+		preset.add(veryfast,undefined);
+		preset.add(faster,undefined);
+		preset.add(fast,undefined);
+		preset.value = "faster";
+	}
+	if(resolution.value == "1280x720") {
+		while (preset.options.length > 2) {
+			preset.remove(2);
+		}
+		preset.add(veryfast,undefined);
+		preset.add(faster,undefined);
+		preset.value = "veryfast";
+	} 
+	if(resolution.value == "1920x1080") {
+		while (preset.options.length > 2) {
+			preset.remove(2);
+		}
+		preset.value = "superfast";
+	}
+}
+
+function set_auto() {
+	if(auto.checked == true){
+		url.style.visibility = "visible"
+		urlTag.style.visibility = "visible"
+		rtmpurl.style.visibility = "hidden"
+		rtmpurlTag.style.visibility = "hidden"
+		rtmpkey.style.visibility = "hidden"
+		rtmpkeyTag.style.visibility = "hidden"
+		vbitrate.style.visibility = "hidden"
+		vbitrateTag.style.visibility = "hidden"
+		abitrate.style.visibility = "hidden"
+		abitrateTag.style.visibility = "hidden"
+	} else {
+		url.style.visibility = "hidden"
+		urlTag.style.visibility = "hidden"
+		rtmpurl.style.visibility = "visible"
+		rtmpurlTag.style.visibility = "visible"
+		rtmpkey.style.visibility = "visible"
+		rtmpkeyTag.style.visibility = "visible"
+		vbitrate.style.visibility = "visible"
+		vbitrateTag.style.visibility = "visible"
+		abitrate.style.visibility = "visible"
+		abitrateTag.style.visibility = "visible"
+	}
+}
+
+set_presets();
+set_auto();
+resolution.addEventListener("change", set_presets);
+auto.addEventListener("change", set_auto);
 submit.addEventListener("click", write);
 status_btn.addEventListener("click", () => {service_ctl("status")});
 stop.addEventListener("click", () => {service_ctl("stop")});
 start.addEventListener("click", () => {service_ctl("start")});
 restart.addEventListener("click", () => {service_ctl("restart")});
 
-cockpit.transport.wait(() => {});

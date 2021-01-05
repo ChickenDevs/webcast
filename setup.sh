417a34d47e684a47bf36181fb9be47696d051307
ChickenDevs <chickenthef1sh@gmail.com>
2021-01-05 08:05:48 -0700

    Update setup.sh
    #3

--- setup.sh
+++ setup.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 sudo cp code/webcast.* /etc/systemd/system
 sudo cp -r code/webcast /etc
+sudo chown -R webcast:webcast /etc/webcast
+chmod 664 /etc/webcast/webcast.conf
+chmod 775 /etc/webcast/webcast.py
 sudo cp -r code/webcastconfig /usr/share/cockpit
 sudo systemctl daemon-reload
 sudo systemctl enable webcast.timer
